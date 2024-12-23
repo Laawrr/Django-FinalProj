366af19152a705c54bb15e190ee1541289313715
Laawrr <lawrencekie8@gmail.com>
2024-12-23 11:51:18 +0800

    finalizm

--- finalProject1/messages_app/forms.py
+++ finalProject1/messages_app/forms.py
@@ -8,7 +8,7 @@ class MessageForm(forms.ModelForm):
 
     content = forms.CharField(
         widget=forms.TextInput(attrs={
-            'class': 'flex-grow px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm',
+            'class': 'flex-1 px-4 py-2 bg-white border border-gray-200 rounded-lg focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500',
             'placeholder': 'Type a message...'
         })
     )
